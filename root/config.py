d0be3285fc63e5bba4fb4fc38ef503c028c18d75
Manjunath Bhat <86521081+Bhatmanjusms@users.noreply.github.com>
2022-11-05 09:59:29 +0530

    Update config.py

--- root/config.py
+++ root/config.py
@@ -9,13 +9,13 @@ import os
 
 
 class Config(object):
-    APP_ID = int(os.environ.get("APP_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
+    APP_ID = int(os.environ.get("APP_ID", "8813038"))
+    API_HASH = os.environ.get("API_HASH", "780fd96b159baa710dada78ff1621b54")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5708128216:AAFmjBLGKcJMp86dKawfZX5qBMPVc1U-2Lc")
     AUTH_USERS = [int(x) for x in os.environ.get("AUTH_USERS", "").split()]
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./bot/DOWNLOADS")
-    DB_URI = os.environ.get("DATABASE_URL")
+    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://manju:manju@cluster0.ufpy5w1.mongodb.net/?retryWrites=true&w=majority")
     # owner is for log cmd only owner can use (this can be multiple users)
-    OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID").split(" ")]
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "BotDunia")
+    OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID").split("2083503061")]
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "jack sparrow")
     CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", False)
